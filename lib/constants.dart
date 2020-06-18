ec1f82cef6a56ac9a88c4207d2d74b1a18022fb4
Saad Bakht <rotk13@gmail.com>
2020-06-18 15:21:09 -0400

    update to first person

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,7 +6,7 @@ import 'enums.dart';
 const kSelectedColor = Color(0xFF03A9F4);
 const kSecondaryTextColor = Colors.grey;
 const kBackgroundColor = Colors.white;
-const kTextBefore = "Did you";
+const kTextBefore = "Did I";
 const kTextAfterToday = "today?";
 const kTextAfterYesterday = "yesterday?";
 //const kAnswerTextStyle = TextStyle(fontSize: 60);
